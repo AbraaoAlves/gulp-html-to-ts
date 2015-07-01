152ac2039bd0f2e0755e375f21f752a137ba6d73
Abraão Alves Teodosio Filho <abraao_alves@outlook.com>
2015-07-01 11:56:10 -0300

    make sample and gulp task

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var html2ts = require('./index');
+
+gulp.task('template', function() {
+	return gulp.src( 'samples/template.html' )
+	.pipe(html2ts() )
+	.pipe(gulp.dest( './samples' ));
+});
