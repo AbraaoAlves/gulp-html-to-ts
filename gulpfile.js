79585eba29f935e0588e7b9b94fd2f38a06a8cc1
Richard Lasjunies <rlasjunies@gmail.com>
2015-07-04 10:52:06 +0200

    more use cases (TS Template, srcType, destType)
    * support of TS Template
    * support of different source type
    * support of different destination type

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,37 @@ var gulp = require('gulp');
 var html2ts = require('./index');
 
 gulp.task('template', function() {
+	//Default values
 	return gulp.src( 'samples/template.html' )
-	.pipe(html2ts())
-	.pipe(gulp.dest( './samples' ));
+		.pipe(html2ts())
+		.pipe(gulp.dest( './samples' ));
 });
 
-gulp.task('template:withPrefix', function() {
-	return gulp.src( 'samples/template.html' )
-	.pipe(html2ts('app'))
-	.pipe(gulp.dest( './samples' ));
-});
-
-gulp.task('template:config', function() {
+gulp.task('template:configureTSTemplate', function() {
+	//A different TS template
 	return gulp.src( 'samples/template.html' )
 		.pipe(html2ts({
-			moduleName: 'app.{$folderName}', //or '$fileName' or 'stringFixed'
-			propertyName: '{$fileName}Html'
+			tsTemplate: "//AUTO GENERATED DO NOT MODIFIED HERE \n namespace app.$folderName { export var $fileName = \'$fileContent\';}"
+		}))
+		.pipe(gulp.dest( './samples' ));
+});
+
+gulp.task('template:configureSrcType', function() {
+	//Different source file type (if not all *.HTML files are template to transform)
+	return gulp.src( 'samples/template.htm' )   //source stream is different .HTM vs .HTML
+		.pipe(html2ts({
+			fileSrcType: ".htm",
+			tsTemplate: "module mySPA { export var template = \'$fileContent\';}"
+		}))
+		.pipe(gulp.dest( './samples' ));
+});
+
+gulp.task('template:configureDestType', function() {
+	//Why not produce javascript files either?
+	return gulp.src( 'samples/template.html' )
+		.pipe(html2ts({
+			fileDestType: ".js",
+			tsTemplate: "var app = exports = module.exports = { template:\'$fileContent\'}"
 		}))
 		.pipe(gulp.dest( './samples' ));
 });
